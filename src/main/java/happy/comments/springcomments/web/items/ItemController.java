31bec8b9410d19b827932ecf7679a006c618a388
Imaspear <rjsckdd12@gmail.com>
2022-02-06 22:56:16 +0900

    feat : 댓글 추가, 수정 기능 구현

--- src/main/java/happy/comments/springcomments/web/items/ItemController.java
+++ src/main/java/happy/comments/springcomments/web/items/ItemController.java
@@ -1,6 +1,7 @@
 package happy.comments.springcomments.web.items;
 
 import happy.comments.springcomments.domain.Item;
+import happy.comments.springcomments.domain.Member;
 import happy.comments.springcomments.domain.comment.Comment;
 import happy.comments.springcomments.repository.CommentRepository;
 import happy.comments.springcomments.repository.ItemRepository;
@@ -25,7 +26,7 @@ public class ItemController {
 
     @GetMapping("/{itemId}")
     public String itemDetail(@PathVariable Long itemId, Model model) {
-        log.info("id {}", itemId);
+        log.info("id : {}", itemId);
         Item item = itemRepository.findById(itemId).stream().findFirst().orElse(null);
         if (item == null) {
             return "redirect:/";
@@ -36,12 +37,32 @@ public class ItemController {
     }
 
     @PostMapping("/comments/edit")
-    public String editComment(@ModelAttribute EditForm editForm) {
-        return "";
+    public String editComment(@RequestBody EditForm editForm) {
+        log.info("editForm : [{}][{}]",editForm.getMember_id(), editForm.getContent());
+        try {
+            Comment editComment = commentRepository.findById(editForm.getComment_id()).orElseThrow(NoSuchFieldError::new);
+            Member member = memberRepository.findById(editForm.getMember_id()).orElseThrow(NoSuchFieldError::new);
+            if (editComment.checkMember(member)) {
+                editComment.updateComment(editForm.getContent());
+                commentRepository.save(editComment);
+            }
+        } catch (Exception e) {
+            log.error("[{}][{}]",this.getClass(), e.getMessage());
+        }
+        return "redirect:/items/"+editForm.getItem_id();
     }
 
     @PostMapping("/comments/add")
-    public String addComment(){
-        return "";
+    public String addComment(@RequestBody AddForm addForm){
+        log.info("addForm : [{}][{}]",addForm.getMember_id(), addForm.getContent());
+        try {
+            Item item = itemRepository.findById(addForm.getItem_id()).orElseThrow(NoSuchFieldError::new);
+            Member member = memberRepository.findById(addForm.getMember_id()).orElseThrow(NoSuchFieldError::new);
+            Comment comment = new Comment(member, item, addForm.getContent());
+            commentRepository.save(comment);
+        } catch (Exception e) {
+            log.error("[{}][{}]",this.getClass(), e.getMessage());
+        }
+        return "redirect:/items/"+addForm.getItem_id();
     }
 }
